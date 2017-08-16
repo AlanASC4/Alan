9cbdea8c0668e8b571507d0fcfe8aa0c7402345d
AlanASC4 <alan.torres0229@gmail.com>
2017-08-16 19:10:30 -0400

    updating

--- demoDayProject/feed.js
+++ demoDayProject/feed.js
@@ -1,112 +1,36 @@
+$(function(){
 var database = firebase.database().ref();
 var ref = firebase.database().ref('users');
-var refuser;
-var locations = [];
-database.on("child_added", function(rowData){
-    var user = firebase.auth().currentUser;
-    var email = user.email;
-    var specificKey;
+var golocations = [];
+database.on("child_changed", function(rowData){
+    golocations = [];
+    console.log(true);
     
    var users = rowData.val();
     var keys = Object.keys(users);
-   for (var i = 0; i < keys.length; i++){
-            var k = keys[i];
-            if( email == users[k].email){
-                specificKey = k;
-                }
-            }
-    refuser = users[specificKey];
-    console.log(refuser);
-    // console.log('this',specificKey);
-    // alert(1);
-    // var row = rowData.val();
-    // var name = row.NAME;
-    // var message = row.MESSAGE;
-    // var fullText = "<p>" + name + " : " + message + "</p>";
-    // $(".allMessages").append(fullText);
-    // console.log(rowData);
-    // console.log(rowData.val());
-    if (rowData.val()[specificKey].ping == undefined){
-        console.log('theres nothing you dummy');
+    console.log(keys)
+    var prevalues = [];
+    for (k = 0; k < keys.length; k++){
+        prevalues.push(users[keys[k]].ping)
     }
-    else {
-        var values = Object.values(rowData.val()[specificKey].ping);
-        for (v = 0; v < values.length; v++){
-            locations.push(values[v]);
+    console.log('pre',prevalues);
+    for (p = 0; p < prevalues.length; p++){
+        // console.log('inbtw', prevalues[p]);
+        if (prevalues[p] == undefined){}
+        else {
+        var push = Object.values(prevalues[p])
+        for (pu = 0; pu < push.length; pu++){
+            golocations.push(push[pu]);
         }
-        console.log('locations',locations);
-        //would refuser.ping work just the same?
-        console.log (rowData.val()[specificKey].ping);
+        }
     }
-    // console.log('p',locations);
+    $('#feed').empty();
+    console.log('global locations',glocations);
+    for (g=golocations.length-1; g>=0; g--){
+        $("#feed").append("<div class = 'jumbotron'><p> Lat:" + 
+        golocations[g].Lat  +"</p> <p> Long:" + golocations[g].Long  +
+        "</p>" + "<p> Desc:"+ golocations[g].Desc   +"</p>" + "<hr><p> Name:" + golocations[g].Name  +"</p></div>");
+        console.log(golocations[g].Lat)
+    }
 });
-function pop(){
-    console.log(refuser)
-}
-function displayInfo(){
-    $(".container").empty();
-    findName();
-    findPing();
-}
-function errData(err){
-    console.log('Error!');
-    console.log(err);
-}
-//finds name/nickname and displays it
-function findName(){
-    ref.on('value', findName2, errData)
-}
-function findName2(data){
-    var user = firebase.auth().currentUser;
-    var userid = user.uid;
-    var specificKey;
-   var users = data.val();
-//    console.log(users);
-    var keys = Object.keys(users);
-   for (var i = 0; i < keys.length; i++){
-            var k = keys[i];
-            if( userid == users[k].uid){
-                specificKey = k;
-                }
-            }
-    var userData = users[specificKey];
-    
-    $(".container").append("<p>" + userData.name  +"</p>")
-}
-//finds recent pings and displays it 
-function findPing(){
-    ref.on('value', findPing2, errData)
-}
-function findPing2(data){
-    var user = firebase.auth().currentUser;
-    var userid = user.uid;
-    var specificKey;
-   var users = data.val();
-//    console.log(users);
-    var keys = Object.keys(users);
-   for (var i = 0; i < keys.length; i++){
-            var k = keys[i];
-            if( userid == users[k].uid){
-                specificKey = k;
-                }
-            }
-    var userData = users[specificKey];
-    var values = Object.keys(userData.ping);
-    console.log('values',values);
-    for (v = 0; v < values.length; v++){
-                $("#feed").append("<div class='jumbotron'> <h2> Title:" + userData.ping[values[v]].Title  + 
-                "</h2><br><p>Lat:" + userData.ping[values[v]].Lat  + "<br>Long:" + 
-                userData.ping[values[v]].Long +"<br>Desc:" + userData.ping[values[v]].Desc  
-                + "<br><hr><p>Pinged by: " + userData.name + "</p>");
-                
-            
-                // $(".container").append("<p> Lat:" + userData.ping[values[v]].Lat  +"</p>");
-            
-                // $(".container").append("<p> Long:" + userData.ping[values[v]].Long  +"</p>");
-                
-                // $(".container").append("<p> Desc:" + userData.ping[values[v]].Desc  +"</p>");
-                $(".container").append("<p> </p>")
-            
-        
-    }
-}
+});
--- demoDayProject/script.js
+++ demoDayProject/script.js
@@ -1,49 +1,124 @@
 var database = firebase.database().ref();
 var ref = firebase.database().ref('users');
+
 //makes the date buggy
 var time = new Date();
 var today = new Date();
 var dd = today.getDate();
 var mm = today.getMonth()+1; //January is 0!
 var yyyy = today.getFullYear();
+
 if(dd<10) {
     dd = '0'+dd
 } 
+
 if(mm<10) {
     mm = '0'+mm
 } 
+
 today = mm + '/' + dd + '/' + yyyy;
 // makes the date
+
 function errData(err){
     console.log('Error!');
     console.log(err);
 }
+
+
 //storing data into firebase and linking it to the current user via his/her email;
+
 //need a function that returns the users key in the database "KYIUEWJK@KJ"
     //that way any other function would iterate through the databse and when it finds a matching key will
         //do something (...)
+
+
+
 function searchPing() {
     var userSearch = document.getElementById("searchPing").value;
     var tag = document.getElementById("tagFilter").value;
     document.getElementById("response").innerHTML = "You searched for " + userSearch + " in " + tag;
 }
-function showFilter(id) {
-    var e = document.getElementById(id);
-    if (e.style.display == 'block')
-        e.style.display = 'none';
-    else
-        e.style.display = 'block';
+
+var num = 0; // To test if is program first tiem running
+var tagArray = ["guitar", "games", "beach", "xbox", "party", "football", "groupwork", "restaurant", "fundraiser", "soupkitchen", "newhouse", "comicbooks", "boardgame", "yoga", "cook"];
+function addTag(id) {
+    var newTag = document.getElementById(id).value;
+    for (var i = 0; i < tagArray.length; i++) {
+        if (newTag.toUpperCase() == tagArray[i].toUpperCase()) {
+            return;
+        }
+        
+    }
+    tagArray.push(newTag);
 }
-// function editMarker(){
-//   var newtitle = prompt("Please enter The title of your ping:", "ping title");
-//     if (newtitle != null) {
-//         title = newtitle;}
-//   var newdesc = prompt("Please enter a brief description of your ping:", "ping desc");
-//     if (newdesc != null) {
-//         desc = newdesc;}
-// }
+function updateTagField() {
+    for (var i = 0; i < tagArray.length; i++) {
+        $(".tagChoiceField").append('<label class="tags btn btn-secondary">'
+    + '<input type="checkbox" onclick="addTagsToCollection(' + i + ');" autocomplete="off" id="tag' + i + '">' + tagArray[i] + '</label>');
+    }
+}
+function showFilter() {
+    var e = document.getElementById('popUpFilter');
+    if (e.style.display == 'block') {
+        e.style.display = 'none';
+    }
+    else {
+        e.style.display = 'block';
+        updateTagField();
+        console.log("I am here");
+    }
+}
+var pingTags;
+var temp = [];
+function addTagsToCollection(num) {
+    temp.push(tagArray[num]);
+}
+function addTagsToPing() {
+    pingTags = temp;
+    temp = [];
+    for (var i = 0; i < pingTags.length; i++) {
+        $(".addedToPing").append(pingTags[i]);
+        console.log(pingTags[i]);
+    }
+    pingTags = [];
+}
+// clear tags in pop up window when submit or x button is clicked
+function clearTags() {
+    $(".tagChoiceField").empty();
+}
+function contains(text1, text2) {
+    if (text1.indexOf(text2) != -1) {
+        return true;
+    } else return false;
+}
+$(function() { 
+    $("#newTag").keyup(function() {
+        console.log("key is up");
+        var searchText = $("#newTag").val().toUpperCase();
+        if (searchText == "") {
+            $(".tagChoiceField .tags").each(function() {
+                $(this).show();
+            });
+        }
+        $(".tagChoiceField .tags").each(function() {
+                if (contains($(this).text().toUpperCase(), searchText)) {
+                    console.log($(this).text());
+                    $(this).show();
+    
+                } 
+                else {
+                    $(this).hide();
+                    console.log($(this).text());
+                }
+        });
+    });
+});
+
+
 var locations = [];
 var increment = 0;
+
+
 database.on("child_added", function(rowData){
     var user = firebase.auth().currentUser;
     var email = user.email;
@@ -56,6 +131,7 @@ database.on("child_added", function(rowData){
                 specificKey = k;
                 }
             }
+
     if (rowData.val()[specificKey].ping == undefined){
         console.log('theres nothing you dummy');
     }
@@ -70,12 +146,17 @@ database.on("child_added", function(rowData){
     // console.log('p',locations);
     increment = locations.length;
 });
+
+
+
 function initMap() {
 var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 6,
-  center: new google.maps.LatLng(39.809860, -98.555183),
+  center: new google.maps.LatLng(),
   mapTypeId: google.maps.MapTypeId.ROADMAP
+
 });
+var geocoder = new google.maps.Geocoder;
 var infowindow = new google.maps.InfoWindow();
 var marker;
 for (var i = 0; i < locations.length; i++) {  
@@ -86,18 +167,22 @@ for (var i = 0; i < locations.length; i++) {
     draggable: true,
     position: new google.maps.LatLng(locations[i].Lat, locations[i].Long),
     map: map, 
+
   });
   google.maps.event.addListener(marker, 'click', (function(marker, i) {
     return function() {
       infowindow.open(map, marker);
       //
+
+
       infowindow.setContent('<div contentEditable="true" ' +
                                    'style="height: 100px; color: black;">' +
                                    'Title: '+ locations[i].Title +'<br>'  +
                                     'Description: ' + locations[i].Desc + '<br>' +
                                     'Date: ' + locations[i].Date + '<br>' + 
                                     'Duration: ' + locations[i].Duration + '<br>' + 
-                                    "<button type='button' class='btn btn-info' data-toggle='collapse' data-target='#demo' onclick='showComments({ " + 'Time:' + locations[i].Time + ',' + 'Uid:' + ' "' + locations[i].Uid + '" ' + ","  +     "})'>"   + "Show comments </button>" + "<br>" +
+                                    'Address: ' + locations[i].Address + '<br>' +
+                                    "<button onclick='showComments({ " + 'Time:' + locations[i].Time + ',' + 'Uid:' + ' "' + locations[i].Uid + '" ' + ","  +     "})'>"   + "Show comments </button>" + "<br>" +
                  
                                       '</div>');
       
@@ -105,44 +190,97 @@ for (var i = 0; i < locations.length; i++) {
   })(marker, i), function() {
          marker.openInfoWindowHtml();
       });
-  
+  getglobalData();
+  globalUpdate();
 }//console.log(marker);  
       }
 
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+
+    var mylat = position.coords.latitude;
+    var mylong = position.coords.longitude;
+    $('#Lat').val(mylat);
+    $('#Long').val( mylong);
+    $('#Latlng').val(mylat + "," + mylong )
+        console.log(mylat, mylong, true);
+
+}
 
 
+function geocodeLatLng(geocoder, map, infowindow) {
+    var geocoder = new google.maps.Geocoder;
+        var input = document.getElementById('Latlng').value;
+        var latlngStr = input.split(',', 2);
+
+        var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+        console.log(latlng)
+        geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[0]) {
+
+              $('#Address').val(results[0].formatted_address);
+              
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+      }
 
 
 function addMarker(){
     ref.once('value', addMarker1, errData);
 }
+
 function addMarker1(data){
+
   var user = firebase.auth().currentUser;
     // console.log(user);
     var userid = user.uid;
     // console.log('www', userid)
     var specificKey;
+
    var users = data.val();
     var keys = Object.keys(users);
+
    for (var i = 0; i < keys.length; i++){
             var k = keys[i];
             if( userid == users[k].uid){
                 specificKey = k;
                 }
             }
+
     var refping = firebase.database().ref('users/' + specificKey + '/ping');
+
+
+
 locations.push({
     Uid: userid,
     Title: $('#title').val(), 
-    Lat: Math.random() * 10 + 30, 
-    Long: Math.random() * 10 - 100,
+    Lat: $('#Lat').val(), 
+    Long: $('#Long').val(),
     Desc: $("#desc").val(),
      Date: today,
     Name: user.displayName,
     Duration: $('#dur').val(),
     Comments: {First: 'Beat you to it'},
     Time: time.getTime(),
+    Address: String($('#Address').val())
+
 });
+
 refping.push({
     Uid: locations[increment].Uid,
     Title: locations[increment].Title,
@@ -154,16 +292,25 @@ refping.push({
     Duration: locations[increment].Duration,
     Comments: locations[increment].Comments ,
     Time: locations[increment].Time,
+    Address: locations[increment].Address
+
 } );
+
 initMap();
 increment++;
 // console.log(locations)
+
 }
+
+
+
+
 //adds new users
 function newUser(){
 ref.on('value', gotData, errData);
 }
 //good stuff; this adds new users
+
 function gotData(data){
     
     //gets current user; also good stuff
@@ -209,6 +356,7 @@ function gotData(data){
         // // looks through database for user, if uid is found push into database as a new user, otherwise do nothing
     }
 }
+
 //Golbal locations
 var glocations = [];
 function getglobalData(){
@@ -220,6 +368,7 @@ database.once("child_added", function(rowData){
     for (k = 0; k < keys.length; k++){
         prevalues.push(users[keys[k]].ping)
     }
+
     for (p = 0; p < prevalues.length; p++){
         // console.log('inbtw', prevalues[p]);
         if (prevalues[p] == undefined){}
@@ -233,11 +382,15 @@ database.once("child_added", function(rowData){
     // console.log('global locations',glocations);
 });
 }
+
 //adds all pings to Map
+
+
 function globalUpdate(){
     ref.once('value', global, errData)
 }
 var glocations = []
+
 function addComment(ping){
     var pinglist;
     var refcomment;
@@ -253,6 +406,7 @@ function addComment(ping){
             // console.log(ping.Uid);
             
             if (rowData.val()[keys[k]].uid == ping.Uid){
+
                 pinglist = Object.keys(rowData.val()[keys[k]].ping);
                 console.log(pinglist.length, 'd')
                 // console.log('work');
@@ -274,14 +428,18 @@ function addComment(ping){
                     }
                     
                 }
+
                 break
             }
         }
+
     },
     
     )
 }
+
 function showComments(ping){
+    $('#demo').empty()
     var pinglist;
     var refcomment;
     //established connectsion bewteen firebase and ping
@@ -300,25 +458,31 @@ function showComments(ping){
                         for (c = 0; c < comments.length; c++){
                             if (c!= comments.length-1){
                                 console.log(rowData.val()[keys[k]].ping[pinglist[pl]].Comments[comments[c]]);
+                                $('#demo').append(rowData.val()[keys[k]].ping[pinglist[pl]].Comments[comments[c]].name + ":" + rowData.val()[keys[k]].ping[pinglist[pl]].Comments[comments[c]].message)
                             }
                         }
                         break
                     }
                 }
+
                 break
             }
         }
+
     },
     
     )
 }
+
+
 function global(data) {
     var user = firebase.auth().currentUser;
     var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 6,
-    center: new google.maps.LatLng(39.809860, -98.555183),
+    zoom: 12,
+    center: new google.maps.LatLng(40.730610, -73.935242),
     mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+});
+    var geocoder = new google.maps.Geocoder;
     var infowindow = new google.maps.InfoWindow();
     var marker;
     for (var i = 0; i < glocations.length; i++) {  
@@ -330,21 +494,35 @@ function global(data) {
     google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
         infowindow.open(map, marker);
-        // console.log(user.displayName)
+        console.log(user.displayName)
      infowindow.setContent('<div contentEditable="true" ' +
                                    'style="height: 100px; color: black;">' +
                                    'Title: '+ glocations[i].Title +'<br>'  +
                                     'Description: ' + glocations[i].Desc + '<br>' +
                                     'Date: ' + glocations[i].Date + '<br>' + 
                                     'Duration: ' + glocations[i].Duration + '<br>' + 
+
+                                    'Address: ' + glocations[i].Address + '<br>' +
+
                                      'posted by ' + glocations[i].Name     + '<br>' +
-                                    "<button onclick='showComments({ " + 'Time:' + glocations[i].Time + ',' + 'Uid:' + ' "' + glocations[i].Uid + '" ' + ","  +     "})'>"   + "Show comments </button>" + "<br>" +
-                                    // "<button onclick="  + + "add comment </button>" + "<br>" +
-                                    "<button type='button' onclick= 'addComment({ " + 'Time:' + glocations[i].Time + ',' + 'Uid:' + ' "' + glocations[i].Uid + '" ' + ","  + 'User:' + ' "' +user.displayName + '" ' + ',' + 'message:' + '5' +     "})'>"   + "Add comment </button>" + "<br>" +
                                      
+                                    "<button onclick='showComments({ " + 'Time:' + glocations[i].Time + ',' + 'Uid:' + ' "' + glocations[i].Uid + '" ' + ","  +     "})'>"   + "Show comments </button>" + "<br>" +
+                                    
+                                    "<button type='button' class='btn btn-info' data-toggle='collapse' data-target='#demo' onclick= 'addComment({ " + 'Time:' + glocations[i].Time + ',' + 'Uid:' + ' "' + glocations[i].Uid + '" ' + ","  + 'User:' + ' "' +user.displayName + '" ' + ',' + 'message:' + '5' +     "})'>"   + "Add comment </button>" + "<br>" +
+                                     
+                                     "<div class='collapse' id='demo'>"   +
+
+
+                                     "</div>"   +
+
                                       '</div>');
       
     }
+
+
+
+
+
     })(marker, i), function() {
             marker.openInfoWindowHtml();
         });
@@ -352,32 +530,43 @@ function global(data) {
     }//console.log(marker);  
 }
 
-// type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo"
-
-
-
 function addglobalMarker(){
     ref.once('value', addglobalMarker1, errData);
 }
+
 function addglobalMarker1(data){
+
   var user = firebase.auth().currentUser;
+    // console.log(user);
     var email = user.email;
     var specificKey;
+
    var users = data.val();
     var keys = Object.keys(users);
+
    for (var i = 0; i < keys.length; i++){
             var k = keys[i];
             if( email == users[k].email){
                 specificKey = k;
                 }
             }
+
     var refping = firebase.database().ref('users/' + specificKey + '/ping');
+
+
 addMarker();
+
 getglobalData();
 globalUpdate();
+
 }
 
+
+
+
+
 //sign in/out
+
 //called when successful user log in
 function onSignIn(googleUser) {
     console.log('User signed in!');
@@ -388,7 +577,9 @@ function onSignIn(googleUser) {
     $(".email").text(profile.getEmail());
    window.location.href = "position.html";
     googleSignin();
+
 }
+
 function onSignIn2(googleUser) {
     console.log('User signed in!');
     var profile = googleUser.getBasicProfile();
@@ -399,8 +590,10 @@ function onSignIn2(googleUser) {
     googleSignin();
     initMap()
 }
+
 //authenticates users?
 var provider = new firebase.auth.GoogleAuthProvider();
+
 function googleSignin() {
    firebase.auth()
   
@@ -408,6 +601,8 @@ function googleSignin() {
       var token = result.credential.accessToken;
       var user = result.user;
       var email = result.user.email;
+
+
   }).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -415,7 +610,9 @@ function googleSignin() {
       console.log(error.message)
    });
   
+
 }
+
 function googleSignout() {
    firebase.auth().signOut()
    .then(function() {
@@ -424,6 +621,7 @@ function googleSignout() {
       console.log('Signout Failed')  
   });
 } 
+
 //called when "sign out" button clicked
 function onSignOut() {
     console.log(gapi.auth2);
@@ -440,4 +638,8 @@ function onSignOut() {
     });
     googleSignout();
 }
+
+// document.getElementById('submit').addEventListener('click', function() {
+//           geocodeLatLng(geocoder, map, infowindow);
+//         });
 //sing in/out
